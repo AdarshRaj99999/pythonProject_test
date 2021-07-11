122bd217dad91cc30569b2b8a1091d67c439a7a4
Adarsh Raj <adarsh.raj@happiestminds.com>
2021-07-11 20:16:31 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import phonenumbers
+from phonenumbers import carrier
+
+number ="+918447983042"
+
+from phonenumbers import geocoder
+ch_number= phonenumbers.parse(number,"CH")
+print(geocoder.description_for_number(ch_number,"en"))
+
+service_number = phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(service_number,"en"))
